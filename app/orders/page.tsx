746126c8e9a65fcdf451771f5a2aba41b7fdcc64
Jainam Sheth <j4inam@gmail.com>
2024-02-29 19:47:30 -0500

    Add get orders query to orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,5 +1,21 @@
-const Orders = () => {
-  return <></>;
+import prismaClient from "@/models/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+const getOrdersList = async (userId?: string) => {
+  const orders = await prismaClient.orders.findMany({
+    where: {
+      userId
+    }
+  });
+
+  return orders;
+};
+
+const Orders = async () => {
+  const { getUser } = getKindeServerSession();
+  let userData = await getUser();
+  const productsList = await getOrdersList(userData?.id);
+  return <>{JSON.stringify(productsList)}</>;
 };
 
 export default Orders;
