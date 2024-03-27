5d07ebdef65ac8c30d0da3758e69bd313e381392
Jainam Sheth <j4inam@gmail.com>
2024-03-27 00:30:49 -0400

    Fix key for orders iterations

--- app/(store)/orders/page.tsx
+++ app/(store)/orders/page.tsx
@@ -56,9 +56,9 @@ const Orders = async () => {
       {ordersList.length !== 0 && (
         <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 mb-2">
           {ordersList.map((order: OrdersSelect) => (
-            <div className="card bg-primary shadow-xl rounded-box w-full">
+            <div className="card bg-primary shadow-xl rounded-box w-full" key={order.id}>
               <div className="card-body p-4">
-                <OrderItem order={order} key={order.id} />
+                <OrderItem order={order} />
               </div>
             </div>
           ))}
