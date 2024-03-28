e1cb0a693bab3740e84517ceeb8d9827542d76a3
Jainam Sheth <j4inam@gmail.com>
2024-03-27 23:30:37 -0400

    Fix linting errors

--- app/(store)/about-us/page.tsx
+++ app/(store)/about-us/page.tsx
@@ -5,7 +5,7 @@ const AboutUs = () => {
         <div className="card-body py-4">
           <h1 className="text-2xl font-bold">Meet Sugar DD!</h1>
           <p className="text-primary-content text-lg text-justify">
-            Sugar DD isn't just a cake shop, it's a place where we turn life's
+            Sugar DD isn&apos;t just a cake shop, it&apos;s a place where we turn life&apos;s
             milestones into delicious memories. We believe that every occasion,
             big or small, deserves a touch of sweetness.
           </p>
@@ -15,22 +15,22 @@ const AboutUs = () => {
         <div className="card-body">
           <h2 className="text-xl font-bold">Meet The Baker</h2>
           <p className="text-accent-content text-lg text-justify">
-            Here at Sugar DD, we're all about passion, creativity, and a whole
-            lot of flavor. Leading the charge is Richa, the heart of Sugar DD's
+            Here at Sugar DD, we&apos;re all about passion, creativity, and a whole
+            lot of flavor. Leading the charge is Richa, the heart of Sugar DD&apos;s
             delectable creations.
           </p>
           <p className="text-accent-content text-lg text-justify">
             Amid the COVID chaos, she found solace in the kitchen, turning
             moments of solitude into culinary adventures. With a background in
-            Hospitality Culinary arts, Richa didn't just bake during COVID – she
+            Hospitality Culinary arts, Richa didn&apos;t just bake during COVID – she
             flourished. It was in those challenging times that her passion for
             baking truly sparked the grand idea of fusion Indian cakes.
           </p>
           <p className="text-accent-content text-lg text-justify">
             Through countless trials and taste tests with family and friends,
             she perfected her craft, unveiling a treasure trove of tempting
-            treats. So, when you bite into a Sugar DD delight, you're not just
-            tasting a cake – you're savoring a story of triumph and sweet
+            treats. So, when you bite into a Sugar DD delight, you&apos;re not just
+            tasting a cake – you&apos;re savoring a story of triumph and sweet
             success baked with love by Richa and our passionate team.
           </p>
         </div>
@@ -40,7 +40,7 @@ const AboutUs = () => {
           <h2 className="text-xl font-bold">Our Story</h2>
           <p className="text-primary-content text-lg text-justify">
             Picture this: a cozy dinner table, a group of friends, and one
-            incredible cake stealing the spotlight. That's where the magic of
+            incredible cake stealing the spotlight. That&apos;s where the magic of
             SugarDD began. One evening, over mouthwatering slices and laughter,
             we had a revelation – why keep these amazing cakes to ourselves when
             the whole world deserves a taste?
@@ -56,7 +56,7 @@ const AboutUs = () => {
         <div className="card-body">
           <h2 className="text-xl font-bold">Our Passion</h2>
           <p className="text-secondary-content text-lg text-justify">
-            We're passionate about using only the finest ingredients to craft
+            We&apos;re passionate about using only the finest ingredients to craft
             cakes that are not only beautiful but also incredibly flavorful.
             From classic favorites to unique creations, we pour our hearts (and
             a sprinkle of magic!) into every cake we bake.
@@ -87,11 +87,11 @@ const AboutUs = () => {
       <div className="card bg-primary my-2 shadow-xl">
         <div className="card-body">
           <h2 className="text-xl font-bold">
-            We'd Love To Celebrate With YOU!
+            We&apos;d Love To Celebrate With YOU!
           </h2>
           <p className="text-primary-content text-lg text-justify">
-            Whether you're planning a birthday party, a wedding, a graduation,
-            or just want a sweet treat, we're here to help. Contact us today to
+            Whether you&apos;re planning a birthday party, a wedding, a graduation,
+            or just want a sweet treat, we&apos;re here to help. Contact us today to
             discuss your cake dreams and let us create a masterpiece that will
             leave a lasting impression.
           </p>
