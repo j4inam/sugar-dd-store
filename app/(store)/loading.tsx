27b7d34a2b12bd967516d1d806e696251df18dd8
Jainam Sheth <j4inam@gmail.com>
2024-03-27 23:01:35 -0400

    Added loading skeletons for pages

--- app/(store)/loading.tsx
+++ app/(store)/loading.tsx
@@ -1,9 +1,22 @@
 import ProductListItemSkeleton from "@/components/ProductListItemSkeleton";
 
-export default function Loading() {
+export default function ProductsLoading() {
   return (
-    <section className="flex flex-col items-center justify-center w-full h-full">
-      <ProductListItemSkeleton />
-    </section>
+    <>
+      <section className="flex flex-col items-center w-full md:hidden lg:mt-4">
+        {[0, 1, 2].map((idx) => (
+          <ProductListItemSkeleton key={idx} />
+        ))}
+      </section>
+      <section className="hidden md:flex flex-col items-center justify-center h-full">
+        <div className="carousel carousel-center sm:w-4/5 xl:w-1/2 p-4 space-x-4 bg-primary rounded-box shadow-xl">
+          {[0, 1, 2, 3].map((idx) => (
+            <div className="carousel-item" key={idx}>
+              <ProductListItemSkeleton />
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
